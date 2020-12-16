1e179b488cd6722bc9d732f9bfddc97fafc69650
rezky08 <rezky.setiawan85@gmail.com>
2020-12-16 22:48:28 +0700

    create telegram table

---
+++ database/migrations/2020_12_16_154437_create_telegram_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTelegramTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('telegrams', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('bot_token', 255);
+            $table->string('name', 100);
+            $table->string('username', 100);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('telegrams');
+    }
+}
