57699e224a421bbe0b29a6087a483bc14835c80f
rezky08 <rezky.setiawan85@gmail.com>
2020-12-17 08:54:45 +0700

    add webhook and delete in controller

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -5,14 +5,20 @@ namespace App\Http\Controllers;
 use App\Helpers\Breadcrumb;
 use App\Helpers\TelegramBot;
 use App\Models\Telegram;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 
 class TelegramController extends Controller
 {
     private $breadcrumbs;
+    private $telegram_model;
     function __construct(Request $request)
     {
         $this->breadcrumbs = (new Breadcrumb)->get($request->path());
+        $this->telegram_model = new Telegram();
     }
     /**
      * Display a listing of the resource.
@@ -21,9 +27,11 @@ class TelegramController extends Controller
      */
     public function index()
     {
+        $user = Auth::user();
+        $telegrams = $user->telegram;
         $data = [
             'title' => "Telegram",
-            'telegrams' => [],
+            'telegrams' => $telegrams,
             'breadcrumbs' => $this->breadcrumbs
         ];
         return view('telegram.telegram_list', $data);
@@ -47,8 +55,51 @@ class TelegramController extends Controller
      */
     public function store(Request $request)
     {
-        dd((new TelegramBot("1231725805:AAGNfddJG9WEEKhxgG5iSn6arrOq0m6qJ0c"))->connectWebhook("https://cupbot1.herokuapp.com/getChat"));
-        // dd($request->all());
+        $rules = [
+            'name' => ['required', 'filled'],
+            'username' => ['required', 'filled'],
+            'token' => ['required', 'filled', 'unique:telegrams,bot_token,NULL,id,deleted_at,NULL']
+        ];
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            $response = [
+                'error' => collect($validator->errors()->messages())->flatten()->first()
+            ];
+            return redirect()->back()->with($response);
+        }
+
+        // $webhook = env('TELEGRAM_WEBHOOK') . $request->token;
+        $webhook = env('TELEGRAM_WEBHOOK');
+        $stat_webhook = (new TelegramBot($request->token))->connectWebhook($webhook);
+        if (!$stat_webhook) {
+            $response = [
+                'error' => 'Sorry, cannot add telegram bot now.'
+            ];
+            return redirect()->back()->with($response);
+        }
+
+        try {
+            $user = Auth::user();
+            $data_insert = [
+                'user_id' => $user->id,
+                'bot_token' => $request->token,
+                'name' => $request->name,
+                'username' => $request->username,
+                'webhook' => $webhook,
+                'created_at' => new \DateTime
+            ];
+            $this->telegram_model->insert($data_insert);
+        } catch (Exception $e) {
+            Log::error($e->getMessage());
+            $response = [
+                'error' => "Internal Server Error 500"
+            ];
+            return redirect()->back()->with($response);
+        }
+        $response = [
+            'success' => "Success add telegram bot " . $request->name . "!"
+        ];
+        return redirect()->back()->with($response);
     }
 
     /**
@@ -91,8 +142,39 @@ class TelegramController extends Controller
      * @param  \App\Models\Telegram  $telegram
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Telegram $telegram)
+    public function destroy($id)
     {
-        //
+        $user = Auth::user();
+        $telegram = $user->telegram->find($id);
+
+        if (!$telegram) {
+            $response = [
+                'error' => "we cannot find your bot"
+            ];
+            return redirect()->back()->with($response);
+        }
+
+        // Disconnect from webhook
+        $stat_webhook = (new TelegramBot($telegram->bot_token))->disconnectWebhook($telegram->webhook);
+        if (!$stat_webhook) {
+            $response = [
+                'error' => 'Sorry, cannot disconnect telegram bot now.'
+            ];
+            return redirect()->back()->with($response);
+        }
+        try {
+            $telegram->delete();
+        } catch (Exception $e) {
+            Log::error($e->getMessage());
+            $response = [
+                'error' => "Internal Server Error 500"
+            ];
+            return redirect()->back()->with($response);
+        }
+
+        $response = [
+            'success' => "Success disconnect telegram bot " . $telegram->name . "!"
+        ];
+        return redirect()->back()->with($response);
     }
 }
