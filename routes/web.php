99d036a06079398566a482a68bd984b21e12dd0f
rezky08 <rezky.setiawan85@gmail.com>
2020-12-21 05:46:06 +0700

    change default redirect

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/dashboard');
 // Route::get('/app/form', 'AppManagerController@create');
 // Route::get('/dashboard', 'AppManagerController@create');
 Route::group(['middleware' => ['auth.redirect:web']], function () {
@@ -30,7 +28,7 @@ Route::group(['middleware' => ['auth.redirect:web']], function () {
 });
 
 Route::group(['middleware' => 'auth'], function () {
-    Route::get('/dashboard', 'AppManagerController@index');
+    Route::get('/dashboard', 'DashboardController@index');
     Route::group(['prefix' => 'application'], function () {
         Route::group(['prefix' => 'key'], function () {
             Route::get('/', 'ApplicationKeyController@index');
@@ -47,15 +45,35 @@ Route::group(['middleware' => 'auth'], function () {
         Route::post('/', 'TelegramController@store');
         Route::delete('/{app_id}/{id}', 'TelegramController@destroy');
         Route::group(['prefix' => 'account'], function () {
-            Route::get('/', 'TelegramAccountController@index');
+            Route::get('/{t_id?}/{id?}', 'TelegramAccountController@index');
         });
         Route::group(['prefix' => 'chat'], function () {
-            Route::get('/', 'TelegramChatController@index');
+            Route::get('/{t_id?}/{id?}', 'TelegramChatController@index');
         });
     });
     Route::group(['prefix' => 'question'], function () {
         Route::get('/{id?}', 'QuestionController@show');
         Route::post('/{id}/bulk', 'QuestionController@bulkStore');
+        Route::get('/{id}/add', 'QuestionController@create');
+        Route::post('/{id}/add', 'QuestionController@store');
+        Route::get('/{id}/{q_id}/edit', 'QuestionController@edit');
+        Route::put('/{id}/{q_id}/edit', 'QuestionController@update');
+        Route::delete('/{id}/{q_id}', 'QuestionController@destroy');
+    });
+    Route::group(['prefix' => 'answer'], function () {
+        Route::get('/{id?}', 'AnswerController@show');
+        Route::post('/{id}/bulk', 'AnswerController@bulkStore');
+        Route::get('/{id}/add', 'AnswerController@create');
+        Route::post('/{id}/add', 'AnswerController@store');
+        Route::get('/{id}/{q_id}/edit', 'AnswerController@edit');
+        Route::put('/{id}/{q_id}/edit', 'AnswerController@update');
+        Route::delete('/{id}/{q_id}', 'AnswerController@destroy');
+    });
+    Route::group(['prefix' => 'label'], function () {
+        Route::get('/{id?}', 'LabelController@show');
+        Route::get('/{id}/add', 'LabelController@create');
+        Route::post('/{id}/add', 'LabelController@store');
+        Route::delete('/{id}/{l_id}', 'LabelController@destroy');
     });
 });
 
