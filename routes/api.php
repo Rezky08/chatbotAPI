7311ae6b1df0ccdc0d4a2d7001a8b27c6c5268ac
rezky08 <rezky.setiawan85@gmail.com>
2020-12-22 16:37:16 +0700

    change route

--- routes/api.php
+++ routes/api.php
@@ -28,8 +28,32 @@ Route::group(['prefix' => 'token'], function () {
 Route::group(['middleware' => ['client:application']], function () {
     Route::group(['prefix' => 'question'], function () {
         Route::get('/', 'API\QuestionController@index');
+        Route::post('/', 'API\QuestionController@store');
     });
     Route::group(['prefix' => 'answer'], function () {
         Route::get('/', 'API\AnswerController@index');
+        Route::post('/', 'API\AnswerController@store');
+    });
+    Route::group(['prefix' => 'label'], function () {
+        Route::get('/', 'API\LabelController@index');
+        Route::post('/', 'API\LabelController@store');
+    });
+    Route::group(['prefix' => 'chat'], function () {
+        Route::get('/', 'API\ChatController@index');
+        Route::post('/', 'API\ChatController@store');
+    });
+    Route::group(['prefix' => 'telegram'], function () {
+        Route::group(['prefix' => 'chat'], function () {
+            Route::get('/', 'API\TelegramChatController@index');
+        });
+        Route::group(['prefix' => 'account'], function () {
+            Route::get('/', 'API\TelegramAccountController@index');
+            Route::post('/', 'API\TelegramAccountController@store');
+        });
+    });
+});
+Route::group(['prefix' => 'telegram'], function () {
+    Route::group(['prefix' => 'chat'], function () {
+        Route::post('{c_id}/{bot_token}', 'API\TelegramChatController@store');
     });
 });
